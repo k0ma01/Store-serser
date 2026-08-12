df74f8cefb8c9251d4098ee74ac7e1882a3048da
k0ma01 <k.oma01@mail.ru>
2026-08-12 23:52:56 +0300

    Created users app, Added User model, Added login and registration views

--- store/settings.py
+++ store/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'products',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -119,9 +120,13 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS=[ BASE_DIR / 'static']
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media' 
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Users
+
+AUTH_USER_MODEL = 'users.User'
