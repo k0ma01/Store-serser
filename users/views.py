ec3ef2fd91071a0d2d0fff01d5795d505783df9c
k0ma01 <k.oma01@mail.ru>
2026-09-02 19:54:52 +0300

    Add the basket logic(calculation of the sum and count)

--- users/views.py
+++ users/views.py
@@ -1,10 +1,10 @@
 from django.contrib import auth,messages
-from django.http import HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponseRedirect
 from django.urls import reverse
+from django.contrib.auth.decorators import login_required
 
 from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
-
+from products.models import Basket
 
 def login(request):
     if request.method == 'POST':
@@ -33,6 +33,7 @@ def registration(request):
     context = {'form': form}
     return render(request, 'users/registration.html', context)
 
+@login_required
 def profile(request):
     if request.method == 'POST':
         form = UserProfileForm(instance=request.user ,data=request.POST,files=request.FILES)
@@ -42,7 +43,11 @@ def profile(request):
             return HttpResponseRedirect(reverse('users:profile'))
     else:
            form = UserProfileForm(instance=request.user)
-    context = {'title': 'Store - Профиль', 'form': form}
+
+    context = {'title': 'Store - Профиль',
+               'form': form,
+               'baskets':Basket.objects.filter(user=request.user),
+               }
     return render(request, 'users/profile.html', context)
 
 def logout(request):
