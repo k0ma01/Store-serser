51a5dcdca03e7fd561fddd2f3a3102ac9c17b391
k0ma01 <k.oma01@mail.ru>
2026-09-11 19:41:02 +0300

    Add UserLoginView,UserRegistrationView classes and LogoutView

--- products/views.py
+++ products/views.py
@@ -1,23 +1,34 @@
 from django.shortcuts import render,HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
+from django.views.generic.base import TemplateView
 
 from products.models import Product,ProductCategory,Basket
-from users.models import User
+from django.views.generic.list import ListView
 
-def index(request):
-    context = {
-        'title': 'Store',
-    }
-    return render(request, 'products/index.html', context)
+class IndexView(TemplateView):
+    template_name = 'products/index.html'
+
+    def get_context_data(self, **kwargs):
+        context=super(IndexView,self).get_context_data()
+        context['title']='Store'
+        return context
 
 
-def products(request):
-    context = {
-        'title': 'Store-Каталог',
-        'products': Product.objects.all(),
-        'categories': ProductCategory.objects.all(),
-    }
-    return render(request, 'products/products.html', context)
+class ProductsListView(ListView):
+    model = Product
+    template_name = 'products/products.html'
+    paginate_by = 2
+
+    def get_queryset(self):
+        queryset = super(ProductsListView, self).get_queryset()
+        category_id = self.kwargs.get('category_id')
+        return queryset.filter(category_id=category_id) if category_id else queryset
+
+    def get_context_data(self, **kwargs):
+        context = super(ProductsListView, self).get_context_data()
+        context['title'] = 'Store - Каталог'
+        context['categories'] = ProductCategory.objects.all()
+        return context
 @login_required
 def basket_add(request,product_id):
     product = Product.objects.get(id=product_id)
